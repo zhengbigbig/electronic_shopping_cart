70e188a94cc9b4cf82d2f0e2bf6f08a64ecf5f49
zhengbigbig <780357902@qq.com>
2019-07-04 15:11:36 +0800

    cart

---
+++ src/main/java/com/company/electronic_shopping_chart/web/cart/controller/CartController.java
@@ -0,0 +1,34 @@
+package com.company.electronic_shopping_chart.web.cart.controller;
+
+import com.company.electronic_shopping_chart.web.cart.view.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CartController {
+    @GetMapping("cartItems")
+    public ResponseEntity<ListCartItemsResponse> listCartItems() {
+        return new ResponseEntity<>(new ListCartItemsResponse(), HttpStatus.OK);
+    }
+
+    @GetMapping("cartItems/{cartItemId}")
+    public ResponseEntity<GetCartItemResponse> getCartItem() {
+        return new ResponseEntity<>(new GetCartItemResponse(), HttpStatus.OK);
+    }
+
+    @PostMapping("cartItems")
+    public ResponseEntity<CreateCartItemResponse> createCartItem(@RequestBody CreateCartItemRequest createCartItemRequest) {
+        return new ResponseEntity<>(new CreateCartItemResponse(), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/cartItems/{cartItemId}")
+    public ResponseEntity<UpdateCartItemResponse> updateCartItem(@PathVariable long cartItemId, @RequestBody UpdateCartItemRequest updateCartItemRequest) {
+        return new ResponseEntity<>(new UpdateCartItemResponse(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/cartItems/{cartItemId}")
+    public ResponseEntity deleteCartItem(@PathVariable long cartItemId) {
+        return new ResponseEntity(HttpStatus.ACCEPTED);
+    }
+}
---
+++ src/main/java/com/company/electronic_shopping_chart/web/cart/view/CreateCartItemResponse.java
@@ -0,0 +1,22 @@
+package com.company.electronic_shopping_chart.web.cart.view;
+
+import com.company.electronic_shopping_chart.web.cart.model.CartItem;
+
+public class CreateCartItemResponse {
+    private CartItem item;
+
+    public CreateCartItemResponse() {
+    }
+
+    public CreateCartItemResponse(CartItem item) {
+        this.item = item;
+    }
+
+    public CartItem getItem() {
+        return item;
+    }
+
+    public void setItem(CartItem item) {
+        this.item = item;
+    }
+}
