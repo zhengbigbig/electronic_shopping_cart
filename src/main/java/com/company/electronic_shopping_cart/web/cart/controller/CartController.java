0d2f8de71548fa61090685610dc23ef86a0643f4
zhengbigbig <780357902@qq.com>
2019-07-08 14:55:14 +0800

    完善CartItem CartItemDao CartController

--- src/main/java/com/company/electronic_shopping_cart/web/cart/controller/CartController.java
+++ src/main/java/com/company/electronic_shopping_cart/web/cart/controller/CartController.java
@@ -1,34 +1,76 @@
 package com.company.electronic_shopping_cart.web.cart.controller;
 
+import com.company.electronic_shopping_cart.web.cart.model.CartItem;
+import com.company.electronic_shopping_cart.web.cart.model.CartItemDao;
 import com.company.electronic_shopping_cart.web.cart.view.*;
+import com.company.electronic_shopping_cart.web.product.model.Product;
+import com.company.electronic_shopping_cart.web.product.model.ProductDao;
+import com.company.electronic_shopping_cart.web.user.model.User;
+import com.company.electronic_shopping_cart.web.user.model.UserDao;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class CartController {
-    @GetMapping("cartItems")
+
+    private UserDao userDao;
+    private ProductDao productDao;
+    private CartItemDao cartItemDao;
+
+    public CartController(UserDao userDao, ProductDao productDao, CartItemDao cartItemDao) {
+        this.userDao = userDao;
+        this.productDao = productDao;
+        this.cartItemDao = cartItemDao;
+    }
+
+    @GetMapping("/cartItems")
     public ResponseEntity<ListCartItemsResponse> listCartItems() {
-        return new ResponseEntity<>(new ListCartItemsResponse(), HttpStatus.OK);
+        List<CartItem> items = cartItemDao.findAll();
+        return new ResponseEntity<>(new ListCartItemsResponse(items), HttpStatus.OK);
     }
 
-    @GetMapping("cartItems/{cartItemId}")
+    @GetMapping("/cartItems/{cartItemId}")
     public ResponseEntity<GetCartItemResponse> getCartItem(@PathVariable long cartItemId) {
-        return new ResponseEntity<>(new GetCartItemResponse(), HttpStatus.OK);
+        CartItem item = cartItemDao.getById(cartItemId);
+        if (item == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(new GetCartItemResponse(item), HttpStatus.OK);
+
+        }
     }
 
-    @PostMapping("cartItems")
+    @PostMapping("/cartItems")
     public ResponseEntity<CreateCartItemResponse> createCartItem(@RequestBody CreateCartItemRequest createCartItemRequest) {
-        return new ResponseEntity<>(new CreateCartItemResponse(), HttpStatus.CREATED);
+        User user = userDao.getById(createCartItemRequest.getUserId());
+        Product product = productDao.getById(createCartItemRequest.getProductId());
+        CartItem cartItem = cartItemDao.save(new CartItem(user, product, createCartItemRequest.getQuantity()));
+
+        return new ResponseEntity<>(new CreateCartItemResponse(cartItem), HttpStatus.CREATED);
     }
 
     @PutMapping("/cartItems/{cartItemId}")
     public ResponseEntity<UpdateCartItemResponse> updateCartItem(@PathVariable long cartItemId, @RequestBody UpdateCartItemRequest updateCartItemRequest) {
-        return new ResponseEntity<>(new UpdateCartItemResponse(), HttpStatus.OK);
+        CartItem cartItem = cartItemDao.getById(cartItemId);
+
+        if (cartItem == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        cartItem.setQuantity(updateCartItemRequest.getQuantity());
+        cartItem = cartItemDao.save(cartItem);
+        return new ResponseEntity<>(new UpdateCartItemResponse(cartItem), HttpStatus.OK);
     }
 
     @DeleteMapping("/cartItems/{cartItemId}")
     public ResponseEntity deleteCartItem(@PathVariable long cartItemId) {
+        CartItem cartItem = cartItemDao.getById(cartItemId);
+        if (cartItem == null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        cartItemDao.delete(cartItem);
         return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 }
--- src/main/java/com/company/electronic_shopping_cart/web/cart/model/CartItemDao.java
+++ src/main/java/com/company/electronic_shopping_cart/web/cart/model/CartItemDao.java
@@ -3,7 +3,15 @@ package com.company.electronic_shopping_cart.web.cart.model;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CartItemDao extends CrudRepository <CartItem,Long>{
+import java.util.List;
 
+@Repository
+public interface CartItemDao extends CrudRepository<CartItem, Long> {
+    CartItem getById(long id);
+
+    List<CartItem> findAll();
+
+    CartItem save(CartItem cartItem);
+
+    void delete(CartItem cartItem);
 }
