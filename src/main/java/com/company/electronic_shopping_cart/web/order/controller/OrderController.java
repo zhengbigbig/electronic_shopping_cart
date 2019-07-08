b97cb8de4a63531b026a87635a7cf0c5b063f5ce
zhengbigbig <780357902@qq.com>
2019-07-08 14:55:44 +0800

    完善Order OrderDao OrderController

--- src/main/java/com/company/electronic_shopping_cart/web/order/controller/OrderController.java
+++ src/main/java/com/company/electronic_shopping_cart/web/order/controller/OrderController.java
@@ -3,37 +3,78 @@ package com.company.electronic_shopping_cart.web.order.controller;
 import com.company.electronic_shopping_cart.web.cart.view.CreateCartItemRequest;
 import com.company.electronic_shopping_cart.web.cart.view.UpdateCartItemRequest;
 import com.company.electronic_shopping_cart.web.cart.view.UpdateCartItemResponse;
-import com.company.electronic_shopping_cart.web.order.view.CreateOrderResponse;
-import com.company.electronic_shopping_cart.web.order.view.GetOrderResponse;
-import com.company.electronic_shopping_cart.web.order.view.ListOrdersResponse;
+import com.company.electronic_shopping_cart.web.order.model.Order;
+import com.company.electronic_shopping_cart.web.order.model.OrderDao;
+import com.company.electronic_shopping_cart.web.order.view.*;
+import com.company.electronic_shopping_cart.web.product.model.Product;
+import com.company.electronic_shopping_cart.web.product.model.ProductDao;
+import com.company.electronic_shopping_cart.web.user.model.User;
+import com.company.electronic_shopping_cart.web.user.model.UserDao;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class OrderController {
-    @GetMapping("orders")
-    public ResponseEntity<ListOrdersResponse> listOrders(){
-        return new ResponseEntity<>(new ListOrdersResponse(), HttpStatus.OK);
+
+    private UserDao userDao;
+    private ProductDao productDao;
+    private OrderDao orderDao;
+
+    public OrderController(UserDao userDao, ProductDao productDao, OrderDao orderDao) {
+        this.userDao = userDao;
+        this.productDao = productDao;
+        this.orderDao = orderDao;
     }
 
-    @GetMapping("orders/{orderId}")
+    @GetMapping("/orders")
+    public ResponseEntity<ListOrdersResponse> listOrders(){
+        List<Order> orders = orderDao.findAll();
+        return new ResponseEntity<>(new ListOrdersResponse(orders), HttpStatus.OK);
+    }
+
+    @GetMapping("/orders/{orderId}")
     public ResponseEntity<GetOrderResponse> getOrder(@PathVariable long orderId){
-        return new ResponseEntity<>(new GetOrderResponse(),HttpStatus.OK);
+        Order order = orderDao.getById(orderId);
+        if(order == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new GetOrderResponse(order),HttpStatus.OK);
     }
 
     @PostMapping("orders")
-    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateCartItemRequest createCartItemRequest){
-        return new ResponseEntity<>(new CreateOrderResponse(),HttpStatus.CREATED);
+    public ResponseEntity<CreateOrderResponse> createOrder(@RequestBody CreateOrderRequest createOrderRequest){
+        User user = userDao.getById(createOrderRequest.getUserId());
+        Product product = productDao.getById(createOrderRequest.getProductId());
+        Order order = new Order(user,product,createOrderRequest.getQuantity(),createOrderRequest.getStatus(),createOrderRequest.getAddress());
+        order = orderDao.save(order);
+        return new ResponseEntity<>(new CreateOrderResponse(order),HttpStatus.CREATED);
     }
 
     @PutMapping("orders/{orderId}")
-    public ResponseEntity<UpdateCartItemResponse> updateOrder(@PathVariable long orderId, @RequestBody UpdateCartItemRequest updateCartItemRequest){
-        return new ResponseEntity<>(new UpdateCartItemResponse(),HttpStatus.OK);
+    public ResponseEntity<UpdateOrderResponse> updateOrder(@PathVariable long orderId, @RequestBody UpdateOrderRequest updateOrderRequest){
+        Order order = orderDao.getById(orderId);
+        if(order == null){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        order.setQuantity(updateOrderRequest.getQuantity());
+        order.setAddress(updateOrderRequest.getAddress());
+        order.setStatus(updateOrderRequest.getStatus());
+
+        return new ResponseEntity<>(new UpdateOrderResponse(order),HttpStatus.OK);
     }
 
     @DeleteMapping("orders/{orderId}")
     public ResponseEntity deleteOrder(@PathVariable long orderId){
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+        Order order = orderDao.getById(orderId);
+        if(order == null){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        orderDao.delete(order);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
--- src/main/java/com/company/electronic_shopping_cart/web/order/model/Order.java
+++ src/main/java/com/company/electronic_shopping_cart/web/order/model/Order.java
@@ -1,21 +1,52 @@
 package com.company.electronic_shopping_cart.web.order.model;
 
 import com.company.electronic_shopping_cart.web.product.model.Product;
+import com.company.electronic_shopping_cart.web.user.model.User;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    @Column(name = "quantity")
     private long quantity;
+
+    @Column(name = "status")
     private String status;
+
+    @Column(name = "address")
     private String address;
 
     public Order() {
     }
 
-    public Order(Product product, long quantity, String status, String address) {
+    public Order(User user, Product product, long quantity, String status, String address) {
+        this.user = user;
         this.product = product;
         this.quantity = quantity;
         this.status = status;
         this.address = address;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     public Product getProduct() {
